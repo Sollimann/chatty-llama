e9fae6c120443867be248c62c5a954ff78533437
Sollimann <solkristoffer@gmail.com>
2023-08-28 01:16:01 +0200

    fix async websocket

--- backend/src/api/ws.rs
+++ backend/src/api/ws.rs
@@ -1,59 +1,60 @@
 use crate::handler::model::{run_inference, session_setup};
-use actix::{Actor, StreamHandler};
-use actix_web::{get, web, Error, HttpRequest, HttpResponse};
-use actix_web_actors::ws;
+use actix_web::{web, Error, HttpRequest, HttpResponse};
+use actix_ws::Message;
 use llm::{models::Llama, InferenceSession};
 use std::sync::Arc;
+use tokio_stream::StreamExt;
 
-/// Define our WebSocket actor
-pub struct Conversation {
-    pub messages: Vec<Message>,
-    llama: Arc<Llama>,
-    session: InferenceSession,
-}
-
-pub struct Message {
-    pub user: bool,
-    pub text: String,
-}
-
-impl Actor for Conversation {
-    type Context = ws::WebsocketContext<Self>;
-}
-
-/// Handle incoming messages for WebSocket actor
-impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for Conversation {
-    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
-        if let Ok(ws::Message::Text(text)) = msg {
-            println!("Running inference...");
-            let inference_result = run_inference(&self.llama, &mut self.session, &text);
-            println!("Inference complete!");
-
-            // Send the inference result back to the client
-            let response = format!("Bot: {}", inference_result);
-            ctx.text(response);
-        }
-    }
-}
-
-#[get("/ws/")]
-async fn chat_route(
+pub async fn ws(
     req: HttpRequest,
-    stream: web::Payload,
+    body: web::Payload,
     model: web::Data<Arc<Llama>>,
 ) -> Result<HttpResponse, Error> {
-    println!("ws endpoint!");
+    let (response, mut session, mut msg_stream) = actix_ws::handle(&req, body)?;
     let model = model.as_ref().clone();
-    println!("init session");
-    let session = session_setup(model.clone());
+    let model_clone = model.clone();
 
-    ws::start(
-        Conversation {
-            llama: model,
-            messages: vec![],
-            session,
-        },
-        &req,
-        stream,
-    )
+    let inference_session: InferenceSession =
+        web::block(move || session_setup(model)).await.unwrap();
+
+    let inference_session = Arc::new(std::sync::Mutex::new(inference_session));
+
+    println!("Initialized inference session");
+    actix_rt::spawn(async move {
+        while let Some(Ok(msg)) = msg_stream.next().await {
+            match msg {
+                Message::Ping(bytes) => {
+                    if session.pong(&bytes).await.is_err() {
+                        return;
+                    }
+                }
+                Message::Text(text) => {
+                    let model_for_this_iteration = model_clone.clone();
+                    let session_clone = inference_session.clone();
+
+                    println!("Running inference...");
+
+                    let inference_result = web::block(move || {
+                        let mut locked_session = session_clone.lock().unwrap();
+                        run_inference(&model_for_this_iteration, &mut *locked_session, &text)
+                    })
+                    .await
+                    .unwrap();
+
+                    println!("Finished inference!");
+
+                    let bot_response = format!("Bot: {:?}", inference_result);
+                    if session.text(bot_response).await.is_err() {
+                        return;
+                    }
+                }
+                _ => break,
+            }
+        }
+
+        let _ = session.close(None).await;
+        println!("Disconnecting websocket!");
+    });
+
+    Ok(response)
 }
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,21 +1,12 @@
 pub mod api;
 pub mod handler;
 
-use std::io::Write;
-
 use actix_web::{middleware::Logger, web, App, HttpServer};
-use api::ws::chat_route;
-use handler::model::{inference_callback, load_language_model, session_setup};
-
-fn print_token(t: String) {
-    print!("{t}");
-    let mut stdout = std::io::stdout().lock();
-    stdout.flush().unwrap();
-}
+use handler::model::load_language_model;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    use llm::Model;
+    use crate::api::ws::ws;
     use std::sync::Arc;
     env_logger::init();
 
@@ -23,31 +14,14 @@ async fn main() -> std::io::Result<()> {
     println!("model loaded");
 
     // use the model to generate text from a prompt
-    let mut session = Model::start_session(&llama, Default::default());
-
-    println!("Started session");
-
-    static CHARACTER_NAME: &str = "### Assistant";
-    static USER_NAME: &str = "### Human";
-
-    let persona = "A chat between a human and an assistant.";
-    let history = format!(
-        "{CHARACTER_NAME}:Hello - How may I help you today?\n\
-                {USER_NAME}:What is the capital of France?\n\
-                {CHARACTER_NAME}:Paris is the capital of France.\n"
-    );
-
-    println!("Set up initial prompt");
     let llama = Arc::new(llama);
-    // let session = session_setup(llama.clone());
     let model = web::Data::new(llama);
 
-    println!("Host websocket api!");
     HttpServer::new(move || {
         App::new()
             .wrap(Logger::default())
             .app_data(model.clone())
-            .service(chat_route)
+            .route("/ws/", web::get().to(ws))
     })
     .bind("127.0.0.1:8080")?
     .run()
