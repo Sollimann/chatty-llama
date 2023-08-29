568d0947623d9d9c864288baaf279b9dc04f1183
Sollimann <kristoffer.solberg@cognite.com>
2023-08-29 03:14:54 +0200

    push the chat app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import "./App.css";
 import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
 import {
   MainContainer,
@@ -9,13 +8,82 @@ import {
   MessageInput,
   TypingIndicator,
 } from "@chatscope/chat-ui-kit-react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [messages, setMessages] = useState([
+    {
+      message: "Hello, I'm a Chatty Llama! Ask me anything!",
+      sentTime: "just now",
+      sender: "ChattyLlama",
+    },
+  ]);
+  const [isTyping, setIsTyping] = useState(false);
+  const [ws, setWs] = useState(null);
+
+  useEffect(() => {
+    const websocket = new WebSocket("ws://localhost:8080/ws/");
+
+    websocket.onopen = () => {
+      console.log("Connected to the WebSocket server");
+    };
+
+    websocket.onmessage = (event) => {
+const botResponse = event.data; // Assuming server sends raw string as response
+
+      setMessages((prev) => [
+        ...prev,
+        {
+          message: botResponse,
+          sender: "ChattyLlama",
+        },
+      ]);
+      setIsTyping(false);
+    };
+
+    setWs(websocket);
+
+    return () => {
+      websocket.close();
+    };
+  }, []);
+
+  const handleSend = (message) => {
+    const newMessage = {
+      message,
+      direction: "outgoing",
+      sender: "user",
+    };
+
+    setMessages((prev) => [...prev, newMessage]);
+
+    if (ws && ws.readyState === WebSocket.OPEN) {
+      ws.send(message);
+      setIsTyping(true); // Assume bot is typing after we send a message.
+    }
+  };
 
   return (
-    <div className="App">
-      <h1>Vite + React</h1>
+    <div className="App app-background">
+      <div style={{ position: "relative", height: "800px", width: "700px" }}>
+        <MainContainer>
+          <ChatContainer>
+            <MessageList
+              scrollBehavior="smooth"
+              typingIndicator={
+                isTyping ? (
+                  <TypingIndicator content="Chatty Llama is typing" />
+                ) : null
+              }
+            >
+              {messages.map((message, i) => {
+                return <Message key={i} model={message} />;
+              })}
+            </MessageList>
+            <MessageInput placeholder="Type message here" onSend={handleSend} />
+          </ChatContainer>
+        </MainContainer>
+      </div>
     </div>
   );
 }
